78a9a597800a6919e812579543893722d255d999
Paulo Griiettner <PGriiettner@wescodist.com>
2023-11-03 14:36:47 -0400

    added minimal to the project

--- src/apps/chat/src/main.tsx
+++ src/apps/chat/src/main.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
-import App from './app/App';
+import App from './app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+// ----------------------------------------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <StrictMode>
+  <HelmetProvider>
     <BrowserRouter>
-      <App />
+      <Suspense>
+        <App />
+      </Suspense>
     </BrowserRouter>
-  </StrictMode>,
+  </HelmetProvider>
 );
